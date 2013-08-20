49984e999b2b0201e9d5fbf93d2e1db94ddb40d3
Daniel Fanjul <daniel.fanjul.alcuten@gmail.com>
2013-08-20 15:25:13 +0200

    Add Root, Reader, Writer

--- interface.go
+++ interface.go
@@ -1,4 +1,18 @@
 package gobdb
 
-type Foo struct {
+// The type of objects that will be held in memory.
+type Root interface{}
+
+// An operation that will read some data from a Root.
+// It must be deterministic.
+// It must be gob encodable.
+type Reader interface {
+	Read(Root) interface{}
+}
+
+// An operation that will update and read some data from a Root.
+// It must be deterministic.
+// It must be gob encodable.
+type Writer interface {
+	Write(Root) interface{}
 }
